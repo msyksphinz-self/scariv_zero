4bc5e8dd0b2524dd46dcb83b2cfca1c5990cd363
msyksphinz <msyksphinz.dev@gmail.com>
2021-01-30 16:10:46 +0900

    add simple test case

---
+++ tests/common/common.mk
@@ -0,0 +1,28 @@
+.PHONY: compile dump
+
+CFLAGS =
+CFLAGS += -I./../common
+CFLAGS += -DPREALLOCATE=1
+CFLAGS += -mcmodel=medany
+CFLAGS += -static
+CFLAGS += -std=gnu99
+CFLAGS += -march=rv64g
+CFLAGS += -O2
+CFLAGS += -ffast-math
+CFLAGS += -fno-common
+CFLAGS += -fno-builtin-printf
+CFLAGS += -static
+CFLAGS += -nostdlib
+CFLAGS += -nostartfiles
+CFLAGS += -lm
+CFLAGS += -lgcc
+CFLAGS += -T ./../common/test.ld
+
+all: compile dump
+
+compile: test.elf
+test.elf: test.S
+	riscv64-unknown-elf-gcc $(CFLAGS) -o $@ $^
+
+dump:
+	riscv64-unknown-elf-objdump -D test.elf > test.dmp
