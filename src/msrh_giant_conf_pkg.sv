eca81c779fb2ca93bd0ad81ca34949602f671d6a
msyksphinz <msyksphinz.dev@gmail.com>
2021-03-11 23:36:40 +0900

    add giant config

---
+++ src/msrh_giant_conf_pkg.sv
@@ -0,0 +1,14 @@
+package msrh_conf_pkg;
+
+  localparam ICACHE_DATA_W = 512;
+  localparam DCACHE_DATA_W = 512;
+
+  localparam DISP_SIZE = 16;
+
+  localparam ALU_INST_NUM = 8;
+  localparam LSU_INST_NUM = 4;
+
+  localparam ARITH_DISP_SIZE = 16;
+  localparam MEM_DISP_SIZE = 8;
+
+endpackage // msrh_conf_pkg
