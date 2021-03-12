7b2b92a3ee0bcc66d1558e802401ff8773c7384d
msyksphinz <msyksphinz.dev@gmail.com>
2021-03-12 13:41:07 +0900

    Add Vivado synthesis

---
+++ vivado_syn/synth.tcl
@@ -0,0 +1,41 @@
+set PROJ_NAME msrh_tile_wrapper
+set PROJ_DIR .
+set RTL_ROOT_DIR ../src
+set TOP_NAME msrh_tile_wrapper
+
+create_project -in_memory -part xc7z020clg484-1 $PROJ_NAME $PROJ_DIR
+save_project_as -force $PROJ_NAME $PROJ_DIR/$PROJ_NAME.xpr
+
+set_property parent.project_path ${PROJ_DIR}/${PROJ_NAME}.xpr [current_project]
+set_property default_lib xil_defaultlib [current_project]
+set_property target_language Verilog [current_project]
+set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
+set_property vhdl_version vhdl_2k [current_fileset]
+# add_files ${PROJ_DIR}/${PROJ_NAME}.srcs/sources_1/bd/zed_base/zed_base.bd
+
+# set XDC_LIST ""
+#
+# foreach i $XDC_LIST {
+#   set_property used_in_implementation false [get_files -all ${i} ]
+# }
+
+add_files -norecurse ../src/riscv64_pkg.sv
+import_files ../src/riscv64_pkg.sv
+add_files -norecurse ../src/msrh_standard_conf_pkg.sv
+import_files ../src/msrh_standard_conf_pkg.sv
+
+# add_files -norecurse $RTL_ROOT_DIR
+source filelist.tcl
+import_files
+
+# read_xdc user_const.xdc
+# set_property used_in_implementation false [get_files user_const.xdc]
+
+# read_xdc dont_touch.xdc
+# set_property used_in_implementation false [get_files dont_touch.xdc]
+
+synth_design -top ${TOP_NAME} -part xc7z020clg484-1 -fanout_limit 10000 -flatten_hierarchy rebuilt
+write_checkpoint -force ${TOP_NAME}.dcp
+report_utilization -file ${TOP_NAME}_utilization_synth.rpt -pb ${TOP_NAME}_utilization_synth.pb
+
+exit
