cc5e06fb7fef9133aee610a3167ceba9a56ed0d3
msyksphinz <msyksphinz.dev@gmail.com>
2021-03-23 09:05:55 +0900

    Support shift operations

--- src/msrh_alu_pipe.sv
+++ src/msrh_alu_pipe.sv
@@ -21,8 +21,8 @@ module msrh_alu_pipe
 );
 
   typedef struct packed {
-    logic [2:0] op;
-    logic imm;
+    op_t  op;
+    imm_t imm;
   } pipe_ctrl_t;
 
   msrh_pkg::issue_t                         r_ex0_issue;
@@ -129,9 +129,9 @@ end
       r_ex2_pipe_ctrl <= 'h0;
     end else begin
       r_ex2_rs1_data <= ex1_regread_rs1.data;
-      r_ex2_rs2_data <= r_ex1_pipe_ctrl.imm ? {{(riscv_pkg::XLEN_W-12){r_ex1_issue.inst[31]}}, r_ex1_issue.inst[31:20]} :
+      r_ex2_rs2_data <= r_ex1_pipe_ctrl.imm == IMM_S  ? {{(riscv_pkg::XLEN_W-12){r_ex1_issue.inst[31]}}, r_ex1_issue.inst[31:20]} :
+                        r_ex1_pipe_ctrl.imm == IMM_SH ? {{(riscv_pkg::XLEN_W-5){1'b0}}, r_ex1_issue.inst[24:20]} :
                         ex1_regread_rs2.data;
-
       r_ex2_issue <= r_ex1_issue;
       r_ex2_index <= r_ex1_index;
       r_ex2_pipe_ctrl <= r_ex1_pipe_ctrl;
@@ -142,34 +142,40 @@ assign w_ex2_rs1_selected_data = |w_ex2_rs1_fwd_valid ? w_ex2_rs1_fwd_data : r_e
 assign w_ex2_rs2_selected_data = |w_ex2_rs2_fwd_valid ? w_ex2_rs2_fwd_data : r_ex2_rs2_data;
 
 logic [31: 0] tmp_ex2_result_d;
-assign tmp_ex2_result_d = w_ex2_rs1_selected_data[31:0] + w_ex2_rs2_selected_data[31:0];
+assign tmp_ex2_result_d = r_ex2_pipe_ctrl.op == OP_SIGN_ADD_32 ? w_ex2_rs1_selected_data[31:0] +   w_ex2_rs2_selected_data[31:0] :
+                          r_ex2_pipe_ctrl.op == OP_SRL_32      ? w_ex2_rs1_selected_data[31:0] <<  w_ex2_rs2_selected_data[31:0] :
+                          r_ex2_pipe_ctrl.op == OP_SRA_32      ? w_ex2_rs1_selected_data[31:0] >>> w_ex2_rs2_selected_data[31:0] :
+                          'h0;
 
-  always_ff @(posedge i_clk, negedge i_reset_n) begin
-    if (!i_reset_n) begin
-      r_ex3_result <= 'h0;
-      r_ex3_index <= 'h0;
-      r_ex3_issue <= 'h0;
-    end else begin
-      r_ex3_issue <= r_ex2_issue;
-      r_ex3_index <= r_ex2_index;
+always_ff @(posedge i_clk, negedge i_reset_n) begin
+  if (!i_reset_n) begin
+    r_ex3_result <= 'h0;
+    r_ex3_index <= 'h0;
+    r_ex3_issue <= 'h0;
+  end else begin
+    r_ex3_issue <= r_ex2_issue;
+    r_ex3_index <= r_ex2_index;
 
-      case (r_ex2_pipe_ctrl.op)
-        OP_SIGN_LUI: r_ex3_result <= {{(riscv_pkg::XLEN_W-32){r_ex2_issue.inst[31]}}, r_ex2_issue.inst[31:12], 12'h000};
-        /* verilator lint_off WIDTH */
-        OP_SIGN_AUIPC:  r_ex3_result <= r_ex2_issue.pc_addr +
-                                        {{(riscv_pkg::XLEN_W-32){r_ex2_issue.inst[31]}}, r_ex2_issue.inst[31:12], 12'h000};
-        OP_SIGN_ADD:    r_ex3_result <= w_ex2_rs1_selected_data + w_ex2_rs2_selected_data;
-        OP_SIGN_SUB:    r_ex3_result <= w_ex2_rs1_selected_data - w_ex2_rs2_selected_data;
-        OP_SIGN_ADD_32: r_ex3_result <= {{(riscv_pkg::XLEN_W-32){tmp_ex2_result_d[31]}}, tmp_ex2_result_d[31: 0]};
-        default : r_ex3_result <= {riscv_pkg::XLEN_W{1'b0}};
-      endcase
-    end
+    case (r_ex2_pipe_ctrl.op)
+      OP_SIGN_LUI: r_ex3_result <= {{(riscv_pkg::XLEN_W-32){r_ex2_issue.inst[31]}}, r_ex2_issue.inst[31:12], 12'h000};
+      /* verilator lint_off WIDTH */
+      OP_SIGN_AUIPC:  r_ex3_result <= r_ex2_issue.pc_addr +
+                                      {{(riscv_pkg::XLEN_W-32){r_ex2_issue.inst[31]}}, r_ex2_issue.inst[31:12], 12'h000};
+      OP_SIGN_ADD:    r_ex3_result <= w_ex2_rs1_selected_data + w_ex2_rs2_selected_data;
+      OP_SIGN_SUB:    r_ex3_result <= w_ex2_rs1_selected_data - w_ex2_rs2_selected_data;
+      OP_SIGN_ADD_32, OP_SRL_32, OP_SRA_32:
+        r_ex3_result <= {{(riscv_pkg::XLEN_W-32){tmp_ex2_result_d[31]}}, tmp_ex2_result_d[31: 0]};
+      OP_SRL:         r_ex3_result <= w_ex2_rs1_selected_data >>  w_ex2_rs2_selected_data;
+      OP_SRA:         r_ex3_result <= w_ex2_rs1_selected_data >>> w_ex2_rs2_selected_data;
+      default : r_ex3_result <= {riscv_pkg::XLEN_W{1'b0}};
+    endcase
   end
+end
 
-  assign o_ex3_phy_wr.valid = r_ex3_issue.valid & r_ex3_issue.rd_valid;
-  assign o_ex3_phy_wr.rd_rnid = r_ex3_issue.rd_rnid;
-  assign o_ex3_phy_wr.rd_type = r_ex3_issue.rd_type;
-  assign o_ex3_phy_wr.rd_data = r_ex3_result;
+assign o_ex3_phy_wr.valid = r_ex3_issue.valid & r_ex3_issue.rd_valid;
+assign o_ex3_phy_wr.rd_rnid = r_ex3_issue.rd_rnid;
+assign o_ex3_phy_wr.rd_type = r_ex3_issue.rd_type;
+assign o_ex3_phy_wr.rd_data = r_ex3_result;
 
 assign ex3_done_if.done     = r_ex3_issue.valid;
 assign ex3_done_if.index_oh = r_ex3_index;
