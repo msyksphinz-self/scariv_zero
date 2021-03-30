6529d9cc4abd58bda1279186a8c3881d5dbed588
msyksphinz <msyksphinz.dev@gmail.com>
2021-03-30 13:49:26 +0900

    Initial implementation of CSU

---
+++ src/msrh_csu.sv
@@ -0,0 +1,101 @@
+module msrh_csu
+(
+  input logic i_clk,
+  input logic i_reset_n,
+
+  input logic [msrh_conf_pkg::DISP_SIZE-1:0] disp_valid,
+  disp_if.slave                              disp,
+
+  regread_if.master                          ex1_regread_rs1,
+
+  /* Forwarding path */
+  input msrh_pkg::early_wr_t i_early_wr[msrh_pkg::REL_BUS_SIZE],
+  input msrh_pkg::phy_wr_t   i_phy_wr [msrh_pkg::TGT_BUS_SIZE],
+
+  /* write output */
+  output msrh_pkg::early_wr_t o_ex1_early_wr,
+  output msrh_pkg::phy_wr_t   o_ex3_phy_wr,
+
+  output msrh_pkg::done_rpt_t o_done_report
+);
+
+msrh_pkg::disp_t w_disp_inst[msrh_conf_pkg::DISP_SIZE];
+msrh_pkg::disp_t disp_picked_inst[msrh_conf_pkg::CSU_DISP_SIZE];
+logic [msrh_conf_pkg::CSU_DISP_SIZE-1:0] disp_picked_inst_valid;
+logic [msrh_conf_pkg::DISP_SIZE-1:0] disp_picked_grp_id[msrh_conf_pkg::CSU_DISP_SIZE];
+
+msrh_pkg::issue_t w_rv0_issue;
+logic [msrh_pkg::RV_CSU_ENTRY_SIZE-1:0] w_rv0_index_oh;
+
+done_if #(.RV_ENTRY_SIZE(msrh_pkg::RV_CSU_ENTRY_SIZE)) w_ex3_done_if();
+
+logic         w_ex3_done;
+logic [msrh_pkg::RV_CSU_ENTRY_SIZE-1:0] w_ex3_index;
+
+msrh_disp_pickup
+  #(
+    .PORT_BASE(0),
+    .PORT_SIZE(msrh_conf_pkg::CSU_DISP_SIZE)
+    )
+u_msrh_disp_pickup
+  (
+   .i_disp_valid (disp_valid),
+   .i_disp (disp),
+
+   .o_disp_valid  (disp_picked_inst_valid),
+   .o_disp        (disp_picked_inst),
+   .o_disp_grp_id (disp_picked_grp_id)
+   );
+
+msrh_scheduler
+  #(
+    .ENTRY_SIZE  (msrh_pkg::RV_CSU_ENTRY_SIZE),
+    .IN_PORT_SIZE(msrh_conf_pkg::CSU_DISP_SIZE)
+    )
+u_msrh_scheduler
+  (
+   .i_clk    (i_clk),
+   .i_reset_n(i_reset_n),
+
+   .i_disp_valid(disp_picked_inst_valid),
+   .i_cmt_id    (disp.cmt_id),
+   .i_grp_id    (disp_picked_grp_id),
+   .i_disp_info (disp_picked_inst),
+
+   .i_early_wr(i_early_wr),
+
+   .o_issue(w_rv0_issue),
+   .o_iss_index_oh(w_rv0_index_oh),
+
+   .i_ex0_rs_conflicted    (1'b0),
+   .i_ex0_rs_conf_index_oh ({msrh_pkg::RV_CSU_ENTRY_SIZE{1'b0}}),
+
+   .pipe_done_if(w_ex3_done_if),
+
+   .o_done_report (o_done_report)
+   );
+
+
+msrh_csu_pipe
+  #(
+    .RV_ENTRY_SIZE(msrh_pkg::RV_CSU_ENTRY_SIZE)
+    )
+u_csu_pipe
+  (
+   .i_clk    (i_clk),
+   .i_reset_n(i_reset_n),
+
+   .rv0_issue(w_rv0_issue),
+   .rv0_index(w_rv0_index_oh),
+   .ex1_i_phy_wr(i_phy_wr),
+
+   .ex1_regread_rs1(ex1_regread_rs1),
+
+   .o_ex1_early_wr(o_ex1_early_wr),
+   .o_ex3_phy_wr (o_ex3_phy_wr),
+
+   .ex3_done_if   (w_ex3_done_if)
+   );
+
+
+endmodule  // msrh_csu
---
+++ src/msrh_csu_pipe.sv
@@ -0,0 +1,160 @@
+module msrh_csu_pipe
+  import decoder_csu_ctrl_pkg::*;
+#(
+  parameter RV_ENTRY_SIZE = 32
+  )
+(
+  input logic                       i_clk,
+  input logic                       i_reset_n,
+
+  input                             msrh_pkg::issue_t rv0_issue,
+  input logic [RV_ENTRY_SIZE-1:0]   rv0_index,
+  input                             msrh_pkg::phy_wr_t ex1_i_phy_wr[msrh_pkg::TGT_BUS_SIZE],
+
+ regread_if.master                  ex1_regread_rs1,
+
+  output                            msrh_pkg::early_wr_t o_ex1_early_wr,
+  output                            msrh_pkg::phy_wr_t o_ex3_phy_wr,
+
+  done_if.master   ex3_done_if
+);
+
+typedef struct packed {
+  op_t  op;
+} pipe_ctrl_t;
+
+msrh_pkg::issue_t                        r_ex0_issue;
+logic [RV_ENTRY_SIZE-1: 0] w_ex0_index;
+pipe_ctrl_t                              w_ex0_pipe_ctrl;
+
+pipe_ctrl_t                              r_ex1_pipe_ctrl;
+msrh_pkg::issue_t                        r_ex1_issue;
+logic [RV_ENTRY_SIZE-1: 0] r_ex1_index;
+
+logic [msrh_pkg::TGT_BUS_SIZE-1:0] w_ex2_rs1_fwd_valid;
+logic            [riscv_pkg::XLEN_W-1:0] w_ex2_tgt_data          [msrh_pkg::TGT_BUS_SIZE];
+logic            [riscv_pkg::XLEN_W-1:0] w_ex2_rs1_fwd_data;
+
+logic            [riscv_pkg::XLEN_W-1:0] w_ex2_rs1_selected_data;
+
+pipe_ctrl_t                              r_ex2_pipe_ctrl;
+msrh_pkg::issue_t                        r_ex2_issue;
+logic [RV_ENTRY_SIZE-1: 0]               r_ex2_index;
+logic [riscv_pkg::XLEN_W-1:0]            r_ex2_rs1_data;
+
+msrh_pkg::issue_t                        r_ex3_issue;
+logic [riscv_pkg::XLEN_W-1: 0]           r_ex3_result;
+logic [RV_ENTRY_SIZE-1: 0]               r_ex3_index;
+
+always_comb begin
+  r_ex0_issue = rv0_issue;
+  w_ex0_index = rv0_index;
+end
+
+decoder_csu_ctrl u_pipe_ctrl (
+  .inst(r_ex0_issue.inst),
+  .op  (w_ex0_pipe_ctrl.op)
+);
+
+assign ex1_regread_rs1.valid = r_ex1_issue.valid & r_ex1_issue.rs1_valid;
+assign ex1_regread_rs1.rnid  = r_ex1_issue.rs1_rnid;
+
+always_ff @(posedge i_clk, negedge i_reset_n) begin
+  if (!i_reset_n) begin
+    r_ex1_issue <= 'h0;
+    r_ex1_index <= 'h0;
+    r_ex1_pipe_ctrl <= 'h0;
+  end else begin
+    r_ex1_issue <= r_ex0_issue;
+    r_ex1_index <= w_ex0_index;
+    r_ex1_pipe_ctrl <= w_ex0_pipe_ctrl;
+  end
+end
+
+assign o_ex1_early_wr.valid = r_ex1_issue.valid & r_ex1_issue.rd_valid;
+assign o_ex1_early_wr.rd_rnid = r_ex1_issue.rd_rnid;
+assign o_ex1_early_wr.rd_type = msrh_pkg::GPR;
+
+generate
+  for (genvar tgt_idx = 0; tgt_idx < msrh_pkg::REL_BUS_SIZE; tgt_idx++) begin : rs_tgt_loop
+    assign w_ex2_rs1_fwd_valid[tgt_idx] = r_ex2_issue.rs1_valid & ex1_i_phy_wr[tgt_idx].valid &
+                                          (r_ex2_issue.rs1_type == ex1_i_phy_wr[tgt_idx].rd_type) &
+                                          (r_ex2_issue.rs1_rnid == ex1_i_phy_wr[tgt_idx].rd_rnid) &
+                                          (r_ex2_issue.rs1_rnid != 'h0);   // GPR[x0] always zero
+
+    assign w_ex2_tgt_data[tgt_idx] = ex1_i_phy_wr[tgt_idx].rd_data;
+  end
+endgenerate
+
+bit_oh_or #(
+    .WIDTH(riscv_pkg::XLEN_W),
+    .WORDS(msrh_pkg::TGT_BUS_SIZE)
+) u_rs1_data_select (
+    .i_oh(w_ex2_rs1_fwd_valid),
+    .i_data(w_ex2_tgt_data),
+    .o_selected(w_ex2_rs1_fwd_data)
+);
+
+always_ff @(posedge i_clk, negedge i_reset_n) begin
+  if (!i_reset_n) begin
+    r_ex2_rs1_data <= 'h0;
+
+    r_ex2_issue <= 'h0;
+    r_ex2_index <= 'h0;
+    r_ex2_pipe_ctrl <= 'h0;
+  end else begin
+    r_ex2_rs1_data <= ex1_regread_rs1.data;
+
+    r_ex2_issue <= r_ex1_issue;
+    r_ex2_index <= r_ex1_index;
+    r_ex2_pipe_ctrl <= r_ex1_pipe_ctrl;
+  end
+end
+
+assign w_ex2_rs1_selected_data = |w_ex2_rs1_fwd_valid ? w_ex2_rs1_fwd_data : r_ex2_rs1_data;
+
+
+always_ff @(posedge i_clk, negedge i_reset_n) begin
+  if (!i_reset_n) begin
+    r_ex3_result   <= 'h0;
+    r_ex3_index    <= 'h0;
+    r_ex3_issue    <= 'h0;
+  end else begin
+    r_ex3_issue    <= r_ex2_issue;
+    r_ex3_index    <= r_ex2_index;
+
+    case (r_ex2_pipe_ctrl.op)
+      OP_RW: r_ex3_result <= w_ex2_rs1_selected_data;
+      OP_RS: r_ex3_result <= w_ex2_rs1_selected_data;
+      OP_RC: r_ex3_result <= w_ex2_rs1_selected_data;
+      OP__ : r_ex3_result <= 'h0;   // Unconditional Jump
+      default : r_ex3_result <= 'h0;
+    endcase
+  end
+end
+
+assign o_ex3_phy_wr.valid   = 1'b0;
+assign o_ex3_phy_wr.rd_rnid = r_ex3_issue.rd_rnid;
+assign o_ex3_phy_wr.rd_type = r_ex3_issue.rd_type;
+assign o_ex3_phy_wr.rd_data = 'h0;  // r_ex3_result;
+
+assign ex3_done_if.done     = r_ex3_issue.valid;
+assign ex3_done_if.index_oh = r_ex3_index;
+
+logic [riscv_pkg::VADDR_W-1: 0] w_ex2_offset_uj;
+logic [riscv_pkg::VADDR_W-1: 0] w_ex2_offset_sb;
+
+assign w_ex2_offset_uj = {{(riscv_pkg::VADDR_W-21){r_ex2_issue.inst[31]}},
+                          r_ex2_issue.inst[31],
+                          r_ex2_issue.inst[19:12],
+                          r_ex2_issue.inst[20],
+                          r_ex2_issue.inst[30:21],
+                          1'b0};
+assign w_ex2_offset_sb = {{(riscv_pkg::VADDR_W-13){r_ex2_issue.inst[31]}},
+                          r_ex2_issue.inst[31],
+                          r_ex2_issue.inst[ 7],
+                          r_ex2_issue.inst[30:25],
+                          r_ex2_issue.inst[11: 8],
+                          1'b0};
+
+endmodule // msrh_csu_pipe
