89455bda15a031b7a885e3d2aba3c569849a244a
msyksphinz <msyksphinz.dev@gmail.com>
2021-04-09 09:49:28 +0900

    Fix SRA operation

--- src/msrh_alu_pipe.sv
+++ src/msrh_alu_pipe.sv
@@ -172,7 +172,7 @@ always_ff @(posedge i_clk, negedge i_reset_n) begin
       OP_AND:         r_ex3_result <= w_ex2_rs1_selected_data &   w_ex2_rs2_selected_data;
       OP_SLL:         r_ex3_result <= w_ex2_rs1_selected_data <<  w_ex2_rs2_selected_data[$clog2(riscv_pkg::XLEN_W)-1: 0];
       OP_SRL:         r_ex3_result <= w_ex2_rs1_selected_data >>  w_ex2_rs2_selected_data[$clog2(riscv_pkg::XLEN_W)-1: 0];
-      OP_SRA:         r_ex3_result <= w_ex2_rs1_selected_data >>> w_ex2_rs2_selected_data[$clog2(riscv_pkg::XLEN_W)-1: 0];
+      OP_SRA:         r_ex3_result <= $signed(w_ex2_rs1_selected_data) >>> w_ex2_rs2_selected_data[$clog2(riscv_pkg::XLEN_W)-1: 0];
       default : r_ex3_result <= {riscv_pkg::XLEN_W{1'b0}};
     endcase // case (r_ex2_pipe_ctrl.op)
   end
