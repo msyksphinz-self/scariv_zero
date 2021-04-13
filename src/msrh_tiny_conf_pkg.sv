3e9263382eb132e7ced8fd4de6fc885e73cb94ba
msyksphinz <msyksphinz.dev@gmail.com>
2021-04-13 21:48:53 +0900

    Add new tiny_config

---
+++ src/msrh_tiny_conf_pkg.sv
@@ -0,0 +1,25 @@
+package msrh_conf_pkg;
+
+  localparam ICACHE_DATA_W = 128;
+  localparam DCACHE_DATA_W = 128;
+
+  localparam DISP_SIZE = 2;
+
+  localparam ALU_INST_NUM = 1;
+  localparam LSU_INST_NUM = 1;
+
+  localparam ARITH_DISP_SIZE = 1;
+  localparam MEM_DISP_SIZE   = 1;
+  localparam BRU_DISP_SIZE   = 1;
+  localparam CSU_DISP_SIZE   = 1;
+
+  localparam RV_ALU_ENTRY_SIZE = 4;
+
+  localparam LDQ_SIZE = 4;
+  localparam STQ_SIZE = 4;
+
+  localparam RV_CSU_ENTRY_SIZE = 4;
+
+  localparam RV_BRU_ENTRY_SIZE = 4;
+
+endpackage // msrh_conf_pkg
