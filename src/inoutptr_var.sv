2520fdd4e2bb6b82ca0a4e8e0067450c45e9a8ad
msyksphinz <msyksphinz.dev@gmail.com>
2021-05-25 12:22:01 +0900

    Add inoutptr_var assertion

--- src/inoutptr_var.sv
+++ src/inoutptr_var.sv
@@ -42,4 +42,25 @@ end
 assign o_in_ptr  = r_inptr;
 assign o_out_ptr = r_outptr;
 
+`ifdef SIMULATION
+logic signed [$clog2(SIZE)+1: 0] r_sim_cnt;
+always_ff @ (posedge i_clk, negedge i_reset_n) begin
+  if (!i_reset_n) begin
+    r_sim_cnt <= 'h0;
+  end else begin
+    /* verilator lint_off WIDTH */
+    r_sim_cnt <= r_sim_cnt +
+                 (i_in_valid ? i_in_val : 'h0) -
+                 (i_out_valid ? i_out_val : 'h0);
+  end
+  if (r_sim_cnt < 0) begin
+    $fatal (0, "inoutptr_var must not be minus");
+  end
+  if (r_sim_cnt >= SIZE) begin
+    $fatal (0, "inoutptr_var must not be exceeded %d", SIZE);
+  end
+end
+
+`endif // SIMULATION
+
 endmodule // inoutptr
