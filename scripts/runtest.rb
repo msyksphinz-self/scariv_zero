19a3214dd54c5b246c18df1c2fc57f87a4ff4dca
msyksphinz <msyksphinz.dev@gmail.com>
2021-08-01 17:08:53 +0900

    Update regression test scripts

--- scripts/runtest.rb
+++ scripts/runtest.rb
@@ -5,6 +5,8 @@ require "json"
 require "fileutils"
 
 veri_sim_binary = ARGV[0]
+max_threads = ARGV[1].to_i
+
 if veri_sim_binary.include?("rv32") then
   test_json_file = "rv32-tests.json"
 elsif veri_sim_binary.include?("rv64") then
@@ -23,28 +25,37 @@ log_dir = 'log/'
 
 FileUtils.mkdir_p(log_dir)
 
-test_num = 0
 pass_num = 0
 
-$test_table.each{ |test|
-  if test.key?("skip") and test["skip"] == 1 then
-    next
-  end
-  output_file = log_dir + File.basename(test["elf"], ".*") + ".log"
-  command_str = "./" + ARGV[0] + " -e " + "../tests/" + test["elf"] + " -o " + output_file
-  system("#{command_str} 2> /dev/null 1> /dev/null")
-  print test["name"] + "\t: "
-  result_stdout = `cat #{output_file}`
-  if result_stdout.include?("SIMULATION FINISH : FAIL")
-    print "ERROR\n"
-  elsif result_stdout.include?("SIMULATION FINISH : PASS")
-    print "PASS\n"
-    pass_num = pass_num + 1
-  else
-    print "UNKNOWN\n"
-  end
-  test_num = test_num +1
-}
+select_test = $test_table.select{ |test| not (test.key?("skip") and test["skip"] == 1) }
+test_num = select_test.size
+
+puts_locks = Queue.new
+1.times { puts_locks.push :lock }
+
+select_test.each_slice(max_threads) do |group|
+  group.map do |test|
+    Thread.new do
+      output_file = log_dir + File.basename(test["elf"], ".*") + ".log"
+      command_str = "./" + ARGV[0] + " -e " + "../tests/" + test["elf"] + " -o " + output_file
+      system("#{command_str} 2> /dev/null 1> /dev/null")
+
+      lock = puts_locks.pop
+      print test["name"] + "\t: "
+      result_stdout = `cat #{output_file}`
+      if result_stdout.include?("SIMULATION FINISH : FAIL")
+        print "ERROR\n"
+      elsif result_stdout.include?("SIMULATION FINISH : PASS")
+        print "PASS\n"
+        pass_num = pass_num + 1
+      else
+        print "UNKNOWN\n"
+      end
+      puts_locks.push(lock)
+    end
+  end.each(&:join)
+end
+
 
 printf("============================\n")
 printf("PASS / TOTAL = %d / %d\n", pass_num, test_num)
