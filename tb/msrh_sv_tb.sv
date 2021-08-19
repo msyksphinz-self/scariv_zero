fd8ebbed7370a2734526ca19a5d11cdc0ff2fa6d
msyksphinz <msyksphinz.dev@gmail.com>
2021-08-19 14:17:52 +0900

    Remove json dumper temporary #3

--- tb/msrh_sv_tb.sv
+++ tb/msrh_sv_tb.sv
@@ -5,6 +5,8 @@ import "DPI-C" function load_binary
  input string filename,
  input logic is_load_dump
 );
+
+import "DPI-C" function open_log_fp ();
 `endif // DIRECT_LOAD_HEX
 
 module tb;
@@ -333,8 +335,8 @@ initial begin
     $display("+ELF= is not specified");
     $finish(1);
   end
-
-  // load_binary("", filename, 1'b1);
+  open_log_fp();
+  load_binary("", filename, 1'b1);
 `endif // DIRECT_LOAD_HEX
 
 end
@@ -367,52 +369,52 @@ always_ff @ (negedge w_clk, negedge w_msrh_reset_n) begin
 end
 
 
-always_ff @ (negedge w_clk, negedge w_msrh_reset_n) begin
-  if (!w_msrh_reset_n) begin
-  end else begin
-    $fwrite(pipe_fp, "%t PC=%010x | ", $time, u_msrh_tile_wrapper.u_msrh_tile.w_iq_disp.pc_addr);
-    // Schedule Pipe
-    for (int grp_idx = 0; grp_idx < msrh_conf_pkg::DISP_SIZE; grp_idx++) begin
-      $fwrite(pipe_fp, "(");
-      if (u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rd_valid)
-        $fwrite(pipe_fp, "%03d,", u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rd_rnid);
-      else
-        $fwrite(pipe_fp, "   ,");
-      if (u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs1_valid)
-        $fwrite(pipe_fp, "%01d,%03d,", u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs1_ready,
-                u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs1_rnid);
-      else
-        $fwrite(pipe_fp, "     ,");
-      if (u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs2_valid)
-        $fwrite(pipe_fp, "%01d,%03d,", u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs2_ready,
-                u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs2_rnid);
-      else
-        $fwrite(pipe_fp, "     ,");
-      $fwrite(pipe_fp, ")");
-    end
-    $fwrite(pipe_fp, " | ");
-    if (u_msrh_tile_wrapper.u_msrh_tile.u_rob.w_out_valid) begin
-      for (int grp_idx = 0; grp_idx < msrh_conf_pkg::DISP_SIZE; grp_idx++) begin
-        if (committed_rob_entry.grp_id[grp_idx]) begin
-          $fwrite (pipe_fp, "(%02d,%02d) PC=%08x ",
-                   u_msrh_tile_wrapper.u_msrh_tile.u_rob.w_out_cmt_id, 1 << grp_idx,
-                   (committed_rob_entry.pc_addr << 1) + (4 * grp_idx));
-          if (committed_rob_entry.inst[grp_idx].rd_valid) begin
-            $fwrite (pipe_fp, "GPR[%02d](%03d)=%016x : ",
-                     committed_rob_entry.inst[grp_idx].rd_regidx,
-                     committed_rob_entry.inst[grp_idx].rd_rnid,
-                     w_physical_gpr_data[committed_rob_entry.inst[grp_idx].rd_rnid]);
-          end else begin
-            $fwrite (pipe_fp, "                                        : ");
-          end
-          $fwrite (pipe_fp, "DASM(%08x)", committed_rob_entry.inst[grp_idx].inst);
-        end // if (committed_rob_entry.grp_id[grp_idx])
-      end // for (int grp_idx = 0; grp_idx < msrh_conf_pkg::DISP_SIZE; grp_idx++)
-    end // if (u_msrh_tile_wrapper.u_msrh_tile.u_rob.w_entry_all_done[u_msrh_tile_wrapper.u_msrh_tile.u_rob.w_out_cmd_id])
-    $fwrite(pipe_fp, "\n");
-  end // else: !if(!w_msrh_reset_n)
-end // always_ff @ (negedge w_clk, negedge w_msrh_reset_n)
-
-`include "tb_json_dumper.sv"
+// always_ff @ (negedge w_clk, negedge w_msrh_reset_n) begin
+//   if (!w_msrh_reset_n) begin
+//   end else begin
+//     $fwrite(pipe_fp, "%t PC=%010x | ", $time, u_msrh_tile_wrapper.u_msrh_tile.w_iq_disp.pc_addr);
+//     // Schedule Pipe
+//     for (int grp_idx = 0; grp_idx < msrh_conf_pkg::DISP_SIZE; grp_idx++) begin
+//       $fwrite(pipe_fp, "(");
+//       if (u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rd_valid)
+//         $fwrite(pipe_fp, "%03d,", u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rd_rnid);
+//       else
+//         $fwrite(pipe_fp, "   ,");
+//       if (u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs1_valid)
+//         $fwrite(pipe_fp, "%01d,%03d,", u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs1_ready,
+//                 u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs1_rnid);
+//       else
+//         $fwrite(pipe_fp, "     ,");
+//       if (u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs2_valid)
+//         $fwrite(pipe_fp, "%01d,%03d,", u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs2_ready,
+//                 u_msrh_tile_wrapper.u_msrh_tile.w_sc_disp.inst[grp_idx].rs2_rnid);
+//       else
+//         $fwrite(pipe_fp, "     ,");
+//       $fwrite(pipe_fp, ")");
+//     end
+//     $fwrite(pipe_fp, " | ");
+//     if (u_msrh_tile_wrapper.u_msrh_tile.u_rob.w_out_valid) begin
+//       for (int grp_idx = 0; grp_idx < msrh_conf_pkg::DISP_SIZE; grp_idx++) begin
+//         if (committed_rob_entry.grp_id[grp_idx]) begin
+//           $fwrite (pipe_fp, "(%02d,%02d) PC=%08x ",
+//                    u_msrh_tile_wrapper.u_msrh_tile.u_rob.w_out_cmt_id, 1 << grp_idx,
+//                    (committed_rob_entry.pc_addr << 1) + (4 * grp_idx));
+//           if (committed_rob_entry.inst[grp_idx].rd_valid) begin
+//             $fwrite (pipe_fp, "GPR[%02d](%03d)=%016x : ",
+//                      committed_rob_entry.inst[grp_idx].rd_regidx,
+//                      committed_rob_entry.inst[grp_idx].rd_rnid,
+//                      w_physical_gpr_data[committed_rob_entry.inst[grp_idx].rd_rnid]);
+//           end else begin
+//             $fwrite (pipe_fp, "                                        : ");
+//           end
+//           $fwrite (pipe_fp, "DASM(%08x)", committed_rob_entry.inst[grp_idx].inst);
+//         end // if (committed_rob_entry.grp_id[grp_idx])
+//       end // for (int grp_idx = 0; grp_idx < msrh_conf_pkg::DISP_SIZE; grp_idx++)
+//     end // if (u_msrh_tile_wrapper.u_msrh_tile.u_rob.w_entry_all_done[u_msrh_tile_wrapper.u_msrh_tile.u_rob.w_out_cmd_id])
+//     $fwrite(pipe_fp, "\n");
+//   end // else: !if(!w_msrh_reset_n)
+// end // always_ff @ (negedge w_clk, negedge w_msrh_reset_n)
+//
+// `include "tb_json_dumper.sv"
 
 endmodule // tb
