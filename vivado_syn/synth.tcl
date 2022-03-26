3c08c03c3a68f9bfd4879d80266f2b98ee82db63
msyksphinz <msyksphinz.dev@gmail.com>
2022-03-26 22:39:09 +0900

    Update Vivado Synthesis environment

--- vivado_syn/synth.tcl
+++ vivado_syn/synth.tcl
@@ -12,6 +12,7 @@ set_property target_language Verilog [current_project]
 set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
 set_property vhdl_version vhdl_2k [current_fileset]
 
+add_files -norecurse ../src/riscv_common_pkg.sv
 add_files -norecurse ../src/riscv64_pkg.sv
 import_files ../src/riscv64_pkg.sv
 add_files -norecurse ../src/msrh_standard_conf_pkg.sv
@@ -29,6 +30,7 @@ set_property used_in_implementation false [get_files synth_constraints.xdc]
 
 synth_design -top ${TOP_NAME} -part xc7z020clg484-1 -fanout_limit 10000 \
     -flatten_hierarchy rebuilt \
+    -include_dir ../src/fpnew/src/common_cells/include \
     -include_dir ../src \
     -verilog_define RV64
 write_checkpoint -force ${TOP_NAME}.dcp
