3278b4e7174872ae134732157b3c43e431fc6de7
msyksphinz <msyksphinz.dev@gmail.com>
2022-04-10 00:37:08 +0900

    Fix tb_commit_mon_utils fp length #150

--- tb/tb_commit_mon_utils.sv
+++ tb/tb_commit_mon_utils.sv
@@ -33,7 +33,7 @@ committed_entry
 
 
 logic [riscv_pkg::XLEN_W-1: 0] w_physical_int_data [msrh_pkg::RNID_SIZE + 32];
-logic [riscv_pkg::XLEN_W-1: 0] w_physical_fp_data  [msrh_pkg::RNID_SIZE + 32];
+logic [riscv_pkg::FLEN_W-1: 0] w_physical_fp_data  [msrh_pkg::RNID_SIZE + 32];
 generate for (genvar r_idx = 0; r_idx < msrh_pkg::RNID_SIZE; r_idx++) begin: reg_loop
   assign w_physical_int_data[r_idx] = u_msrh_tile_wrapper.u_msrh_tile.u_int_phy_registers.r_phy_regs[r_idx];
   if (riscv_pkg::FLEN_W != 0) begin
