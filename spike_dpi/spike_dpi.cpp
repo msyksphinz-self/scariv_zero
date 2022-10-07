2ad5aa26dd1658031045817be09239237ca27e12
msyksphinz-self <msyksphinz.dev@gmail.com>
2022-10-08 00:40:27 +0900

    spike_dpi.cpp fix CYCLE

--- spike_dpi/spike_dpi.cpp
+++ spike_dpi/spike_dpi.cpp
@@ -793,6 +793,14 @@ void step_spike(long long time, long long rtl_pc,
         fprintf(compare_log_fp, "ISS MCYCLE is updated to RTL = %0*llx\n", g_rv_xlen / 4, rtl_wr_val);
         fprintf(compare_log_fp, "==========================================\n");
         return;
+      } else if (((iss_insn.bits() >> 20) & 0x0fff) == CSR_CYCLE) {
+        p->set_csr(static_cast<int>(CSR_CYCLE), static_cast<reg_t>(rtl_wr_val));
+        p->get_state()->XPR.write(rtl_wr_gpr_addr, rtl_wr_val);
+        fprintf(compare_log_fp, "==========================================\n");
+        fprintf(compare_log_fp, "RTL CYCLE Backporting to ISS.\n");
+        fprintf(compare_log_fp, "ISS CYCLE is updated to RTL = %0*llx\n", g_rv_xlen / 4, rtl_wr_val);
+        fprintf(compare_log_fp, "==========================================\n");
+        return;
       } else if (((iss_insn.bits() >> 20) & 0x0fff) == CSR_MINSTRET) {
         if (rtl_wr_val != iss_wr_val) {
           p->set_csr(static_cast<int>(CSR_MINSTRET), static_cast<reg_t>(rtl_wr_val));
