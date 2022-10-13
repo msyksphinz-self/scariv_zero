37e634cf2b03e5ccddbb59706c5421c9d775faa7
msyksphinz <msyksphinz@gmail.com>
2022-10-13 12:57:35 +0900

    Fix for dynamic link loader

---
+++ spike_dpi/spike_dpi.mk
@@ -0,0 +1,18 @@
+
+HAVE_INT128 := yes
+HAVE_DLOPEN := yes
+
+include riscv-isa-sim/riscv.mk
+
+riscv_cc := $(patsubst %.cc, riscv-isa-sim/%.cc, $(riscv_srcs))
+riscv_objs := $(patsubst %.cc, %.o, $(riscv_cc))
+riscv_objs += spike_dpi.o
+
+CLEAN_FILES += libspike_dpi.a
+CLEAN_FILES += spike_dpi.o
+
+spike_dpi.o: spike_dpi.cpp
+	g++ $^ -c -o $@ $(CFLAGS)
+
+libspike_dpi.a: $(riscv_objs)
+	ar rcs $@ $^
