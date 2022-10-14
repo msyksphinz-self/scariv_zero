3f6946e6a949e12857028290065168c9dad62cd1
msyksphinz-self <msyksphinz.dev@gmail.com>
2022-10-15 00:45:25 +0900

    Fix serialdevice.cc

--- spike_dpi/serialdevice.cc
+++ spike_dpi/serialdevice.cc
@@ -38,7 +38,7 @@ bool serialdevice_t::store(reg_t addr, size_t len, const uint8_t* bytes)
   // std::cerr << "serialdevice_t::store called : addr = " << std::hex << addr << '\n';
   if (bytes[0] >= 0x20 ||
       bytes[0] == 0x10 ||  // LF
-      bytes[0] == 0x13 ||  // CR
+      bytes[0] == 0x13     // CR
       ) {
     fprintf (serial_log_fp, "%c", bytes[0]);
     fflush (serial_log_fp);
