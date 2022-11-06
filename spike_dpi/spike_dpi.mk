7fd4517806b26ae0b1c759a88723666524424b66
msyksphinz-self <msyksphinz.dev@gmail.com>
2022-11-06 19:52:18 +0900

    Update Docker regression

--- spike_dpi/spike_dpi.mk
+++ spike_dpi/spike_dpi.mk
@@ -1,3 +1,10 @@
+VERILATOR_HOME = /usr/local/share/verilator
+CFLAGS += -I$(VERILATOR_HOME)/include/vltstd/
+CFLAGS += -I$(abspath riscv-isa-sim)/riscv
+CFLAGS += -I$(abspath riscv-isa-sim)
+CFLAGS += -I$(abspath riscv-isa-sim)/softfloat/
+CFLAGS += -I$(abspath riscv-isa-sim)/fesvr/
+CFLAGS += -I$(abspath ../cpp/)
 
 HAVE_INT128 := yes
 HAVE_DLOPEN := yes
