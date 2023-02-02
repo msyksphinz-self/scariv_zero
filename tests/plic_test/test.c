2501925a1758b17fd168ffd30aa27ebf613b6620
msyksphinz <msyksphinz.dev@gmail.com>
2023-02-02 19:03:28 +0900

    Update PLIC test case

--- tests/plic_test/test.c
+++ tests/plic_test/test.c
@@ -1,18 +1,35 @@
+#include <stdint.h>
+
 #define PLIC_BASE_ADDR         0xc000000
 #define PLIC_SOURCE_BASE_ADDR  (PLIC_BASE_ADDR)
 #define PLIC_PENDING_BASE_ADDR (PLIC_BASE_ADDR + 0x1000)
 #define PLIC_ENABLE_BASE_ADDR  (PLIC_BASE_ADDR + 0x2000)
 #define PLIC_THRESHOLD_ADDR    (PLIC_BASE_ADDR + 0x200000)
 
+#define NUM_OF_SOURCE 8
+
 int plic_reg_rw_test()
 {
-  for (int i = 0; i < 8; i++) {
-    *((volatile unsigned int *)(PLIC_SOURCE_BASE_ADDR + i * sizeof(unsigned int))) = 0xdeadbeef;
-    *(volatile unsigned int *)(PLIC_SOURCE_BASE_ADDR + i * sizeof(unsigned int));
+  // Priority Register Test
+  for (int i = 0; i <= 4 * NUM_OF_SOURCE; i+= sizeof(uint8_t)) {
+    *((volatile uint8_t *)(PLIC_SOURCE_BASE_ADDR + i)) = 0xdeadbeef;
+    *((volatile uint8_t *)(PLIC_SOURCE_BASE_ADDR + i));
+  }
+  for (int i = 0; i <= 4 * NUM_OF_SOURCE; i+= sizeof(uint16_t)) {
+    *((volatile uint16_t *)(PLIC_SOURCE_BASE_ADDR + i)) = 0xdeadbeef;
+    *((volatile uint16_t *)(PLIC_SOURCE_BASE_ADDR + i));
+  }
+  for (int i = 0; i <= 4 * NUM_OF_SOURCE; i+= sizeof(uint32_t)) {
+    *((volatile uint32_t *)(PLIC_SOURCE_BASE_ADDR + i)) = 0xdeadbeef;
+    *((volatile uint32_t *)(PLIC_SOURCE_BASE_ADDR + i));
+  }
+  for (int i = 0; i <= 4 * NUM_OF_SOURCE; i+= sizeof(uint64_t)) {
+    *((volatile uint64_t *)(PLIC_SOURCE_BASE_ADDR + i)) = 0xdeadbeef;
+    *((volatile uint64_t *)(PLIC_SOURCE_BASE_ADDR + i));
   }
 
-  *((volatile unsigned int *)(PLIC_PENDING_BASE_ADDR)) = 0xdeadbeef;
-  *(volatile unsigned int *)(PLIC_PENDING_BASE_ADDR);
+  // *((volatile unsigned int *)(PLIC_PENDING_BASE_ADDR)) = 0xdeadbeef;
+  // *(volatile unsigned int *)(PLIC_PENDING_BASE_ADDR);
 
   return 0;
 }
