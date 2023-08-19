3c6511c091007d698fdc09c7dfc269ef2d742c65
msyksphinz-self <msyksphinz.dev@gmail.com>
2023-08-19 14:08:30 +0900

    Update gshare_model.cpp

--- spike_dpi/gshare_model.cpp
+++ spike_dpi/gshare_model.cpp
@@ -65,7 +65,7 @@ void initial_gshare(long long bhr_length,
   fprintf (gshare_log_fp, "Info : GSHARE length is set %ld\n", iss_bhr_length);
   fprintf (gshare_log_fp, "Info : Cache Block Size is set %ld\n", iss_cache_block_byte_size);
 
-  for(int i = 0; i < bhr_length; i++) {
+  for(int i = 0; i < (1 << iss_bhr_length); i++) {
     bim_block_t *bim_block = new bim_block_t();
     bim_block->bim = new uint8_t [iss_cache_block_byte_size / 2];
     for (int j = 0; j < iss_cache_block_byte_size / 2; j++) {
@@ -98,6 +98,17 @@ std::string to_binString(unsigned int val, const int length)
 }
 
 
+uint8_t update_bim (uint8_t curr, bool taken)
+{
+  if (taken && curr != 3) {
+    return curr + 1;
+  } else if (!taken && curr == 0) {
+    return curr - 1;
+  } else {
+    return curr;
+  }
+}
+
 void step_gshare (long long rtl_time,
                   int rtl_cmt_id, int rtl_grp_id,
                   long long rtl_gshare_bhr)
@@ -108,32 +119,41 @@ void step_gshare (long long rtl_time,
   auto iss_insn    = p->get_state()->insn;
 
   if (is_cond_branch_inst(iss_insn.bits())) {
-    const size_t iss_cache_block_byte_length = log2(iss_cache_block_byte_size);
     const size_t iss_bhr_mask = (1 << iss_bhr_length) - 1;
 
-    size_t bim_array_index = ((iss_pc >> iss_cache_block_byte_length) >> 1) & iss_bhr_mask;
-    size_t bim_block_internal_index = (iss_pc >> 1) & iss_bhr_mask;
+    size_t bim_array_index = (iss_pc >> 1) / iss_cache_block_byte_size;
+    size_t bim_block_internal_index = (iss_pc & (iss_cache_block_byte_size -1)) >> 1;
+    bim_array_index = (bim_array_index ^ iss_bhr) & iss_bhr_mask;
+
+    // fprintf (gshare_log_fp, "bim_array_index = %d, bim_block_internal_index = %d\n",
+    //          bim_array_index,
+    //          bim_block_internal_index);
 
     uint8_t bim_counter = bim_array.at(bim_array_index)->bim[bim_block_internal_index];
     bool iss_predict_taken = (bim_counter >> 1) & 1;
 
     bool is_branch_taken = iss_next_pc != iss_pc + cond_inst_size(iss_insn.bits());
+
+    fprintf (gshare_log_fp, "GSHARE MODEL : PC = %08lx (%02d,%02d), index = (%4x, %2x), MODEL_BHR = %s, predict = %s, result = %s, %s          ",
+             iss_pc,
+             rtl_cmt_id, rtl_grp_id,
+             static_cast<unsigned int>(bim_array_index), static_cast<unsigned int>(bim_block_internal_index),
+             to_binString(iss_bhr & ((1 << iss_bhr_length)-1), iss_bhr_length).c_str(),
+             iss_predict_taken ? "    TAKEN" : "NOT TAKEN",
+             is_branch_taken   ? "    TAKEN" : "NOT TAKEN",
+             iss_predict_taken == is_branch_taken ? "MATCH" : "FAIL ");
+
     iss_bhr = (iss_bhr << 1) | is_branch_taken;
 
-    fprintf(gshare_log_fp, "%lld : PC=%08lx (%02d,%02d) : ", rtl_time, iss_pc, rtl_cmt_id, rtl_grp_id);
     if ((iss_bhr & (1 << iss_bhr_length) - 1) != rtl_gshare_bhr) {
-      fprintf(gshare_log_fp, "Warning : BHR different: RTL = %s, ISS = %s\n",
+      fprintf(gshare_log_fp, "// Warning : BHR different: RTL = %s, ISS = %s\n",
               to_binString(rtl_gshare_bhr, iss_bhr_length).c_str(),
               to_binString(iss_bhr & ((1 << iss_bhr_length)-1), iss_bhr_length).c_str());
     } else {
-      fprintf(gshare_log_fp, "BHR RTL = %s\n", to_binString(rtl_gshare_bhr, iss_bhr_length).c_str());
+      fprintf(gshare_log_fp, "// BHR RTL = %s\n", to_binString(rtl_gshare_bhr, iss_bhr_length).c_str());
     }
 
-    fprintf (gshare_log_fp, "GSHARE MODEL : index = (%x, %x), predict = %s, result = %s, %s\n",
-             static_cast<unsigned int>(bim_array_index), static_cast<unsigned int>(bim_block_internal_index),
-             iss_predict_taken ? "TAKEN" : "NOT TAKEN",
-             is_branch_taken   ? "TAKEN" : "NOT TAKEN",
-             iss_predict_taken == is_branch_taken ? "MATCH" : "FAIL");
-
+    // Update bim_counter
+    bim_array.at(bim_array_index)->bim[bim_block_internal_index] = update_bim (is_branch_taken, bim_counter);
   }
 }
