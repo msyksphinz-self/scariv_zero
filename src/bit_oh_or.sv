31d58aa0ac377347adea5e3f844b2427887df4b4
msyksphinz-self <msyksphinz.dev@gmail.com>
2023-09-08 00:37:22 +0900

    Change bit_oh_or policy

--- src/bit_oh_or.sv
+++ src/bit_oh_or.sv
@@ -8,16 +8,25 @@ module bit_oh_or
 (
   input logic [WORDS-1:0]  i_oh,
   input T                  i_data[WORDS],
+  /* verilator lint_off UNOPTFLAT */
   output T                 o_selected
 );
-  /* verilator lint_off UNOPTFLAT */
-  logic [$bits(T)-1: 0]    w_selected_array[WORDS];
-  assign w_selected_array[0] = {$bits(T){i_oh[0]}} & i_data[0];
-  generate for (genvar i = 1; i < WORDS; i++) begin : oh_loop
-    assign w_selected_array[i] = w_selected_array[i-1] | {$bits(T){i_oh[i]}} & i_data[i];
+  // /* verilator lint_off UNOPTFLAT */
+  // logic [$bits(T)-1: 0]    w_selected_array[WORDS];
+  // assign w_selected_array[0] = {$bits(T){i_oh[0]}} & i_data[0];
+  // generate for (genvar i = 1; i < WORDS; i++) begin : oh_loop
+  //   assign w_selected_array[i] = w_selected_array[i-1] | {$bits(T){i_oh[i]}} & i_data[i];
+  // end
+  // endgenerate
+logic [$clog2(WORDS)-1: 0] idx;
+function automatic logic [$clog2(WORDS)-1: 0] encoder (logic [WORDS-1: 0] in);
+  for (int i = 0; i < WORDS; i++) begin
+    if (in[i]) return i;
   end
-  endgenerate
-  assign o_selected = T'(w_selected_array[WORDS-1]);
+  return 0;
+endfunction // encoder
+
+assign o_selected = ~|i_oh ? 'h0 : i_data[encoder(i_oh)];
 
 endmodule
 
