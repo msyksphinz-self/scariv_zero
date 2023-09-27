b619a3d4b4031cc1576885c1a7d61a9ba04a2db0
msyksphinz-self <msyksphinz.dev@gmail.com>
2023-09-28 00:39:14 +0900

    Fix vmv.v.x test pattern

--- tests/rvv/vmv_v_x/test.c
+++ tests/rvv/vmv_v_x/test.c
@@ -8,22 +8,22 @@ int main ()
   size_t vl;
   for (int i = 0; i < n; i += vl) {
     vl = __riscv_vsetvl_e64m1(n - i);
-    vint64m1_t vx = __riscv_vmv_v_x_i64m1 (i, vl);
+    register volatile vint64m1_t vx = __riscv_vmv_v_x_i64m1 (i, vl);
   }
 
   for (int i = 0; i < n; i += vl) {
     vl = __riscv_vsetvl_e32m1(n - i);
-    vint32m1_t vx = __riscv_vmv_v_x_i32m1 (i, vl);
+    register volatile vint32m1_t vx = __riscv_vmv_v_x_i32m1 (i, vl);
   }
 
   for (int i = 0; i < n; i += vl) {
     vl = __riscv_vsetvl_e16m1(n - i);
-    vint16m1_t vx = __riscv_vmv_v_x_i16m1 (i, vl);
+    register volatile vint16m1_t vx = __riscv_vmv_v_x_i16m1 (i, vl);
   }
 
   for (int i = 0; i < n; i += vl) {
     vl = __riscv_vsetvl_e8m1(n - i);
-    vint8m1_t vx = __riscv_vmv_v_x_i8m1 (i, vl);
+    register volatile vint8m1_t vx = __riscv_vmv_v_x_i8m1 (i, vl);
   }
 
   return 0;
