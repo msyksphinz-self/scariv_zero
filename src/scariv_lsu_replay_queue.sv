e61e1c50abf9851402412661af153d1799d83d8e
msyksphinz-self <msyksphinz.dev@gmail.com>
2023-11-30 01:13:33 +0900

    Fix timing of replay queue

--- src/scariv_lsu_replay_queue.sv
+++ src/scariv_lsu_replay_queue.sv
@@ -56,10 +56,13 @@ typedef struct packed {
   logic [REPLAY_QUEUE_W-1: 0] diff_counter;
 } queue_t;
 typedef struct packed {
-    logic                valid;
-    logic                dead;
-    scariv_pkg::cmt_id_t cmt_id;
-    scariv_pkg::grp_id_t grp_id;
+  logic                          valid;
+  logic                          dead;
+  scariv_pkg::cmt_id_t           cmt_id;
+  scariv_pkg::grp_id_t           grp_id;
+  scariv_lsu_pkg::ex2_haz_t      hazard_typ;
+  logic [HAZARD_INDEX_SIZE-1: 0] hazard_index;
+  logic                          resolved;
 } replay_additional_queue_t;
 
 queue_t w_new_replay_queue_info;
@@ -110,33 +113,50 @@ end
 logic [REPLAY_QUEUE_W-1: 0] r_replay_queue_head_ptr;
 logic [REPLAY_QUEUE_W-1: 0] r_replay_queue_tail_ptr;
 generate for (genvar idx = 0; idx < REPLAY_QUEUE_SIZE; idx++) begin : replay_additional_loop
-    logic w_commit_flush;
-    logic w_br_flush;
-    logic w_entry_flush;
-    assign w_commit_flush = scariv_pkg::is_flushed_commit(i_commit) & r_replay_additional_queue[idx].valid;
-    assign w_br_flush     = scariv_pkg::is_br_flush_target(r_replay_additional_queue[idx].cmt_id, r_replay_additional_queue[idx].grp_id, br_upd_if.cmt_id, br_upd_if.grp_id,
-                                                           br_upd_if.dead, br_upd_if.mispredict) & br_upd_if.update & r_replay_additional_queue[idx].valid;
-    assign w_entry_flush  = w_commit_flush | w_br_flush;
+  logic w_commit_flush;
+  logic w_br_flush;
+  logic w_entry_flush;
+  assign w_commit_flush = scariv_pkg::is_flushed_commit(i_commit) & r_replay_additional_queue[idx].valid;
+  assign w_br_flush     = scariv_pkg::is_br_flush_target(r_replay_additional_queue[idx].cmt_id, r_replay_additional_queue[idx].grp_id, br_upd_if.cmt_id, br_upd_if.grp_id,
+                                                         br_upd_if.dead, br_upd_if.mispredict) & br_upd_if.update & r_replay_additional_queue[idx].valid;
+  assign w_entry_flush  = w_commit_flush | w_br_flush;
 
-    always_ff @ (posedge i_clk, negedge i_reset_n) begin
-        if (!i_reset_n) begin
-            r_replay_additional_queue[idx].valid <= 1'b0;
+  always_ff @ (posedge i_clk, negedge i_reset_n) begin
+    if (!i_reset_n) begin
+      r_replay_additional_queue[idx].valid <= 1'b0;
+    end else begin
+      if (w_queue_push &
+          (idx == r_replay_queue_head_ptr)) begin
+        r_replay_additional_queue[idx].valid        <= 1'b1;
+        r_replay_additional_queue[idx].dead         <= 1'b0;
+        r_replay_additional_queue[idx].cmt_id       <= lsu_pipe_haz_if.cmt_id;
+        r_replay_additional_queue[idx].grp_id       <= lsu_pipe_haz_if.grp_id;
+        r_replay_additional_queue[idx].hazard_typ   <= lsu_pipe_haz_if.payload.hazard_typ;
+        r_replay_additional_queue[idx].hazard_index <= lsu_pipe_haz_if.payload.hazard_index;
+        r_replay_additional_queue[idx].resolved     <= 1'b0;
+      end else if (w_queue_pop &
+                   (idx == r_replay_queue_tail_ptr)) begin
+        r_replay_additional_queue[idx].valid  <= 1'b0;
+      end else if (r_replay_additional_queue[idx].valid) begin
+        if (w_entry_flush) begin
+          r_replay_additional_queue[idx].dead <= 1'b1;
         end else begin
-            if (w_queue_push &
-                (idx == r_replay_queue_head_ptr)) begin
-                r_replay_additional_queue[idx].valid   <= 1'b1;
-                r_replay_additional_queue[idx].dead    <= 1'b0;
-                r_replay_additional_queue[idx].cmt_id  <= lsu_pipe_haz_if.cmt_id;
-                r_replay_additional_queue[idx].grp_id  <= lsu_pipe_haz_if.grp_id;
-            end else if (w_queue_pop &
-                    (idx == r_replay_queue_tail_ptr)) begin
-                r_replay_additional_queue[idx].valid  <= 1'b0;
-            end else if (r_replay_additional_queue[idx].valid &
-                         w_entry_flush) begin
-                r_replay_additional_queue[idx].dead <= 1'b1;
+          case (r_replay_additional_queue[idx].hazard_typ)
+            EX2_HAZ_STQ_NONFWD_HAZ : r_replay_additional_queue[idx].resolved = (r_replay_additional_queue[idx].hazard_index & ~i_stq_rs2_resolve.index) == 'h0;
+            EX2_HAZ_RMW_ORDER_HAZ  : r_replay_additional_queue[idx].resolved = w_head_is_oldest & i_st_buffer_empty & i_missu_is_empty;
+            EX2_HAZ_L1D_CONFLICT   : r_replay_additional_queue[idx].resolved = 1'b1; // Replay immediately
+            EX2_HAZ_MISSU_FULL     : r_replay_additional_queue[idx].resolved = !i_missu_is_full;
+            EX2_HAZ_MISSU_ASSIGNED : r_replay_additional_queue[idx].resolved = i_missu_resolve.valid & (i_missu_resolve.resolve_index_oh == r_replay_additional_queue[idx].hazard_index) |
+                                                                               ((r_replay_additional_queue[idx].hazard_index & i_missu_resolve.missu_entry_valids) == 'h0);
+            default : begin
+              r_replay_additional_queue[idx].resolved = 1'b0;
+              // $fatal(0, "Must not come here. hazard_typ = %d", w_rd_replay_queue_info.info.hazard_typ);
             end
-        end
-    end
+          endcase // case (r_replay_additional_queue[idx].hazard_typ)
+        end // else: !if(w_entry_flush)
+      end // if (r_replay_additional_queue[idx].valid)
+    end // else: !if(!i_reset_n)
+  end // always_ff @ (posedge i_clk, negedge i_reset_n)
 end endgenerate
 
 always_ff @ (posedge i_clk, negedge i_reset_n) begin
@@ -194,34 +214,25 @@ assign w_head_is_oldest = (rob_info_if.cmt_id == w_replay_additional_queue_tail.
 assign w_replay_additional_queue_tail = r_replay_additional_queue[r_replay_queue_tail_ptr];
 
 always_comb begin
-    if (!w_empty) begin
-        if (w_replay_additional_queue_tail.dead) begin
-            w_lsu_replay_valid = 1'b1;  // immediately remove from queue
-        end else if (w_rd_replay_queue_info.diff_counter != 'h0 &&
-                     r_prev_diff_counter == w_rd_replay_queue_info.diff_counter ||
-                     w_rd_replay_queue_info.diff_counter == 'h0 && (&r_prev_diff_counter) /* Timeout */) begin
-            w_lsu_replay_valid = 1'b1;
-        end else begin
-            case (w_rd_replay_queue_info.info.hazard_typ)
-                EX2_HAZ_STQ_NONFWD_HAZ : w_lsu_replay_valid = (w_rd_replay_queue_info.info.hazard_index & ~i_stq_rs2_resolve.index) == 'h0;
-                EX2_HAZ_RMW_ORDER_HAZ :  w_lsu_replay_valid = w_head_is_oldest & i_st_buffer_empty & i_missu_is_empty;
-                EX2_HAZ_L1D_CONFLICT :   w_lsu_replay_valid = 1'b1; // Replay immediately
-                EX2_HAZ_MISSU_FULL :     w_lsu_replay_valid = !i_missu_is_full;
-                EX2_HAZ_MISSU_ASSIGNED : w_lsu_replay_valid = i_missu_resolve.valid & (i_missu_resolve.resolve_index_oh == w_rd_replay_queue_info.info.hazard_index) |
-                                                              ((w_rd_replay_queue_info.info.hazard_index & i_missu_resolve.missu_entry_valids) == 'h0);
-                default : begin
-                    w_lsu_replay_valid = 1'b0;
-                    // $fatal(0, "Must not come here. hazard_typ = %d", w_rd_replay_queue_info.info.hazard_typ);
-                end
-            endcase
-        end
+  if (!w_empty) begin
+    if (w_replay_additional_queue_tail.dead) begin
+      w_lsu_replay_valid = 1'b1;  // immediately remove from queue
+    end else if (w_replay_additional_queue_tail.valid & w_replay_additional_queue_tail.resolved) begin
+      w_lsu_replay_valid = 1'b1;
+    end else if (w_rd_replay_queue_info.diff_counter != 'h0 &&
+                 r_prev_diff_counter == w_rd_replay_queue_info.diff_counter ||
+                 w_rd_replay_queue_info.diff_counter == 'h0 && (&r_prev_diff_counter) /* Timeout */) begin
+      w_lsu_replay_valid = 1'b1;
     end else begin
-        w_lsu_replay_valid = 1'b0;
+      w_lsu_replay_valid = 1'b0;
     end
+  end else begin
+    w_lsu_replay_valid = 1'b0;
+  end
 
-    lsu_pipe_req_if.cmt_id  = w_replay_additional_queue_tail.cmt_id;
-    lsu_pipe_req_if.grp_id  = w_replay_additional_queue_tail.grp_id;
-    lsu_pipe_req_if.payload = w_rd_replay_queue_info.info          ;
+  lsu_pipe_req_if.cmt_id  = w_replay_additional_queue_tail.cmt_id;
+  lsu_pipe_req_if.grp_id  = w_replay_additional_queue_tail.grp_id;
+  lsu_pipe_req_if.payload = w_rd_replay_queue_info.info          ;
 end // always_comb
 
 assign lsu_pipe_req_if.valid = w_lsu_replay_valid & ~w_replay_additional_queue_tail.dead;
