b56728b6ea8176544cb40e30cc4f7ef3da235702
msyksphinz-self <msyksphinz.dev@gmail.com>
2023-12-15 18:20:14 +0900

    Update riscv-coremark

--- src/scariv_boomv3_conf_pkg.sv
+++ src/scariv_boomv3_conf_pkg.sv
@@ -19,18 +19,18 @@ package scariv_conf_pkg;
   localparam ARITH_DISP_SIZE = 2;
   localparam MULDIV_DISP_SIZE = ARITH_DISP_SIZE / ALU_INST_NUM;
   localparam MEM_DISP_SIZE   = 2;
-  localparam BRU_DISP_SIZE   = 1;
+  localparam BRU_DISP_SIZE   = 2;
   localparam CSU_DISP_SIZE   = 1;
   localparam FPU_DISP_SIZE   = 2;
 
   localparam RV_ALU_ENTRY_SIZE = 20;
 
-  localparam RV_LSU_ENTRY_SIZE = 12;
+  localparam RV_LSU_ENTRY_SIZE = 16;
   localparam LDQ_SIZE = 16;
   localparam STQ_SIZE = 16;
 
   localparam RV_CSU_ENTRY_SIZE = 4;
-  localparam RV_BRU_ENTRY_SIZE = 12;
+  localparam RV_BRU_ENTRY_SIZE = 16;
   localparam RV_FPU_ENTRY_SIZE = 4;
 
   localparam MISSU_ENTRY_SIZE = 2;
