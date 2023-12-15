bf560af0ad8cb1a3ff3bacb3e2ad0d6f6ecdba50
msyksphinz-self <msyksphinz.dev@gmail.com>
2023-12-15 15:16:38 +0900

    Update sniper/spike implementation

--- sniper/build_env.sh
+++ sniper/build_env.sh
@@ -18,7 +18,7 @@ make -j32 SNIPER_ROOT=$SNIPER_ROOT 2>&1 | tee rv8_build.log
 cd ../snipersim
 make BUILD_RISCV=1 -j$(nproc) RV8_HOME=$RV8_HOME 2>&1 | tee sniper_build.log
 
-cd ../riscv-isa-sim
-make clean
-./configure --prefix=${RISCV}  --without-boost --without-boost-asio --without-boost-regex --with-sift=${SNIPER_ROOT} --enable-commitlog
-make -j$(nproc) 2>&1 | tee spike_build.log
+# cd ../riscv-isa-sim
+# make clean
+# ./configure --prefix=${RISCV}  --without-boost --without-boost-asio --without-boost-regex --with-sift=${SNIPER_ROOT} --enable-commitlog
+# make -j$(nproc) 2>&1 | tee spike_build.log
