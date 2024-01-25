a28ca36ab3f38a4502269289120331c31c0cbe97
msyksphinz-self <msyksphinz.dev@gmail.com>
2024-01-25 14:36:25 +0900

    Move config files

--- vivado_syn/synth.tcl
+++ vivado_syn/synth.tcl
@@ -33,8 +33,8 @@ add_files -norecurse ../../src/$::env(RV_FILE)
 import_files ../../src/$::env(RV_FILE)
 
 puts $::env(CONF_FILE)
-add_files -norecurse ../../src/$::env(CONF_FILE)
-import_files ../../src/$::env(CONF_FILE)
+add_files -norecurse ../../configs/$::env(CONF_FILE)
+import_files ../../configs/$::env(CONF_FILE)
 # add_files -norecurse ../../src/scariv_standard_conf_pkg.sv
 # import_files ../../src/scariv_standard_conf_pkg.sv
 
