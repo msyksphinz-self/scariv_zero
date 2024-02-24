eda76a69c6ac4d0780344bab7df107195403e066
msyksphinz-self <msyksphinz.dev@gmail.com>
2024-02-24 13:34:44 +0900

    Fix instruction count from dispatch

--- src/scariv_pkg.sv
+++ src/scariv_pkg.sv
@@ -205,9 +205,9 @@ typedef struct packed {
     logic [ALU_INST_NUM-1: 0][scariv_conf_pkg::DISP_SIZE-1: 0] alu_inst_valid;
     logic [$clog2(MULDIV_DISP_SIZE): 0]                        muldiv_inst_cnt;
     logic [LSU_INST_NUM-1: 0][$clog2(MEM_DISP_SIZE): 0]        lsu_inst_cnt;
-    logic [$clog2(LDQ_SIZE): 0]                                ld_inst_cnt;
-    logic [$clog2(STQ_SIZE): 0]                                st_inst_cnt;
-    logic [scariv_conf_pkg::DISP_SIZE-1: 0]                    lsu_inst_valid;
+    logic [$clog2(MEM_DISP_SIZE): 0]                           ld_inst_cnt;
+    logic [$clog2(MEM_DISP_SIZE): 0]                           st_inst_cnt;
+    logic [LSU_INST_NUM-1: 0][scariv_conf_pkg::DISP_SIZE-1: 0] lsu_inst_valid;
     logic [$clog2(BRU_DISP_SIZE): 0]                           bru_inst_cnt;
     logic [scariv_conf_pkg::DISP_SIZE-1: 0]                    bru_inst_valid;
     logic [scariv_conf_pkg::DISP_SIZE-1: 0]                    bru_branch_valid;
