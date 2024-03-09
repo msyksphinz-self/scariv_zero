7f61a6d1a728a4e347be6d2ace175cd165f6294d
msyksphinz <msyksphinz.dev@gmail.com>
2024-03-09 15:42:47 +0900

    Add vcs_sim env

---
+++ vcs_sim/filelist.f
@@ -0,0 +1,7 @@
+../tb/scariv_sv_tb.sv
+../tb/tb_l2_behavior_ram.sv
+../tb/tb_elf_loader.sv
+../tb/l2c_splitter_wrapper.sv
+../tb/l2c_arbiter_wrapper.sv
+../tb/scariv_serialdevice.sv
+../tb/tb_flash.sv
